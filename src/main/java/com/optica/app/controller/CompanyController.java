f729dc87edef211e308495aba57c5e0fc9fec9d5
Aurindo Neto <aurindo15@gmail.com>
2025-07-04 00:05:31 -0300

    Add CompanyController with createCompany endpoint

--- src/main/java/com/optica/app/controller/CompanyController.java
+++ src/main/java/com/optica/app/controller/CompanyController.java
@@ -1,5 +1,24 @@
 package com.optica.app.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.optica.app.dto.CompanyRequest;
+import com.optica.app.entities.CompanyEntity;
+import com.optica.app.service.CompanyService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/company")
+@AllArgsConstructor
 public class CompanyController {
-  
+  private final CompanyService companyService;
+
+  @PostMapping
+  public CompanyEntity createCompany(@RequestBody CompanyRequest companyRequest) {
+    return companyService.createCompany(CompanyRequest.toEntity(companyRequest));
+  }
 }
